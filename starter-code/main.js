a6be516bf395fe834fb443e6a53895a26630f298
Becky Bolton <Bex@Beckys-MacBook-Air.local>
2016-06-15 21:39:37 +0100

    added sound, scoring

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,9 +1,12 @@
 
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = ['queen', 'king', 'king', 'queen', 'queen', 'king', 'queen', 'king'];
 
 var cardsInPlay = [];
 
 var board = document.getElementById('game-board');
+
+var score = 0;
+
 
 function createBoard() {
 
@@ -35,14 +38,30 @@ function isTwoCards() {
 
 function isMatch(cards) {
 	if (cards[0] === cards[1]) {
-	alert("You found a match!");
- }
- else {
-	alert("Sorry, try again.");
+		score++;
+		document.getElementById('score').innerHTML = score;
+		var audio = new Audio('match.wav');
+		audio.play();
+
+			if (score === 4) {
+			var audio = new Audio('win.wav');
+			audio.play();
+		}
+}
+
+ 	else {
+		var audio = new Audio('wrong.wav');
+		audio.play();
 	}
 }
 
+if (score === 4) {
+	var audio = new Audio('win.wav');
+	audio.play();
+}
+
 createBoard();
+
 
 
 
